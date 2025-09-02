8a166482d39be1b280a1fd26e492ce5da0e9ecd8
anthony-murè <tony.mure99@gmail.com>
2025-09-02 17:22:19 +0200

    [add]Main.jsx(styled-card)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -22,28 +22,24 @@ const Main = () => {
           return(
             
           <div className="col-3" key={actress.id}>
-          <div className="card">
-            <img src={actress.image} className="card-img-top" alt={actress.name}/>
-            <div className="card-body">
-               <p className="card-text">{actress.name}</p>
-               <p className="card-text">{actress.birth_year}</p>
-               <p className="card-text">{actress.nationality}</p>
-               <p className="card-text">{actress.biography}</p>
-               <p className="card-text">{actress.awards}</p>
+            <div className="card">
+              <img src={actress.image} className="card-img-top" alt={actress.name}/>
+                <div className="card-body">
+                  <p className="card-text fw-bold">{actress.name}</p>
+                  <p className="card-text">{actress.birth_year}</p>
+                  <p className="card-text">{actress.nationality}</p>
+                  <p className="card-text">{actress.biography}</p>
+                  <p className="card-text">{actress.awards}</p>
+                </div>
             </div>
           </div>
-        </div>
-
-
 
           );
         })};
         
-
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
 export default Main
