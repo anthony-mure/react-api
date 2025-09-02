951a63dca4adabc7c5c667ec94d1480ea9e5cbbc
anthony-murè <tony.mure99@gmail.com>
2025-09-02 15:50:50 +0200

    [add]Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-
+import Header from "./components/header"
 
 function App() {
-  
+  return (
+    <>
+    <Header />
+    </>
+  )
 }
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+
+const header = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+           <h1 className="text-center">Actresses Cast</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default header
